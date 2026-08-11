026b85f120a6dcd3cceb8a5c85a8b7d3ca7f37d8
gt25963 <gt25963@bristol.ac.uk>
2026-08-11 15:39:49 +0100

    Update submit_fastdesign_C2_cycle2.sh for reseeding
    Reseed fastdesign from the best decoy of cycle 1 to generate new decoys.

--- exploratory/rosetta_alternative_approaches/approach3/submit_fastdesign_C2_cycle2.sh
+++ exploratory/rosetta_alternative_approaches/approach3/submit_fastdesign_C2_cycle2.sh
@@ -8,6 +8,7 @@
 #SBATCH --time=20:00:00
 #SBATCH --output=/scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/logs/fastdesign_C2_cycle2_%j.out
 
+#approach 3 cycle 2: reseeds fastdesign from the best decoy of cycle 1 (decoy_0003.pdb), generating another 20 decoys
 conda run -n pyrosetta python /scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/approach3/fastdesign_membrane.py \
     /scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/main_pipeline/CytbX_C2_approach3/fastdesign_decoys/decoy_0003.pdb \
     /scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/main_pipeline/CytbX_C2_approach3/cycle_2 \
