a242793c25bdab99f5a13334d614a495f3b48946
gt25963 <gt25963@bristol.ac.uk>
2026-08-11 12:32:03 +0100

    run_af3.sh annotation
    Updated comments and organized output directory creation.

--- array_jobs/run_af3.sh
+++ array_jobs/run_af3.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 #SBATCH --job-name=AF3
 #SBATCH --gpus=1
-#SBATCH --time=0:30:00         # Hours:Mins:Secs
+#SBATCH --time=0:30:00 #Hours:Mins:Secs
 #SBATCH --output=/scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/af3_test/log/%j.out
 
 ## Usage: sbatch run_af3.sh <input_path> <output_path>
-
 output_path=$2 
 input_path=$1 
-mkdir -p $output_path
 
+#Set up output directory
+mkdir -p $output_path
 echo $input_path
+
+#Set paths to AF3 model weights, sequence database, and container
 weight_path=$SCRATCH/weights
 database_path=/projects/b5ae/AF3/database/
 af3_container=/projects/b5ae/AF3/af3.sif
 
-
+#Confirm GPU is visible before running AF3
 singularity exec --nv $af3_container bash -c 'nvidia-smi & nvcc --version'
 
+#Run AF3 prediction on all input files, binding local paths into container
 singularity exec \
     --nv \
     --bind $input_path:/opt/af_input \
