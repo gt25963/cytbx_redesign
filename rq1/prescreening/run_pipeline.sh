aae612046e796a245bbbde6723fbe6bb2fdb895a
gt25963 <gt25963@bristol.ac.uk>
2026-08-30 18:15:04 +0100

    Enhance comments in run_pipeline.sh
    Added comments to clarify the purpose of the script.

--- rq1/prescreening/run_pipeline.sh
+++ rq1/prescreening/run_pipeline.sh
@@ -6,7 +6,10 @@
 #SBATCH --mem=8GB
 #SBATCH --output=/scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/logs/pipeline_%j.out
 #SBATCH --account=brics.b5ae
-	
+
+# run_pipeline.sh
+# Wraps master_cytbx_rq1.sh in a sbatch job. 
+# Hence frees up terminal space while running RQ1 on compute node.
 source /home/b5ae/mvg2713124.b5ae/miniconda3/etc/profile.d/conda.sh
 bash /scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/master_cytbx_rq1.sh
 
