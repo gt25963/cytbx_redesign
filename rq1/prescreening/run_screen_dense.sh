d8c6173bfb03c4c508b1ed671b9043eb36840270
gt25963 <gt25963@bristol.ac.uk>
2026-08-30 18:17:42 +0100

    Enhance comments in run_screen_dense.sh
    Added comments to clarify the purpose of the script.

--- rq1/prescreening/run_screen_dense.sh
+++ rq1/prescreening/run_screen_dense.sh
@@ -6,4 +6,7 @@
 #SBATCH --mem=8GB
 #SBATCH --output=/scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/logs/screen_dense_master_%j.out
 #SBATCH --account=brics.b5ae
+
+# run_screen_dense.sh
+# same as run_screen.sh but for the denser RPXDock sampling control. 
 bash /scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/prescreening/master_cytbx_screen_dense.sh
