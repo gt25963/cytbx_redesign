6a1493d8efc6b71e62e231e8bd32e8e4939d06c3
gt25963 <gt25963@bristol.ac.uk>
2026-08-31 21:26:48 +0100

    Add comment to run_rq2_u10.sh script
    Added a comment about node and terminal freeing.

--- rq2/u10/run_rq2_u10.sh
+++ rq2/u10/run_rq2_u10.sh
@@ -6,4 +6,6 @@
 #SBATCH --nodes=1
 #SBATCH --mem=8GB
 #SBATCH --output=/scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/logs/master_RQ2_U10_%j.out
+
+# Again same as all other sbatch scripts - node + freeing terminal
 bash /scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/rq2/master_pipeline/RQ2_U10/master_cytbx_rq2_u10.sh
