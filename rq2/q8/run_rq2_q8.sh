3524ef019a6c29c3b1429a8b0cd209b6ee8e207a
gt25963 <gt25963@bristol.ac.uk>
2026-08-31 21:08:25 +0100

    Update run_rq2_q8.sh

--- rq2/q8/run_rq2_q8.sh
+++ rq2/q8/run_rq2_q8.sh
@@ -6,4 +6,6 @@
 #SBATCH --nodes=1
 #SBATCH --mem=8GB
 #SBATCH --output=/scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/logs/master_RQ2_Q8_%j.out
+
+# Again, sbatch for the q8 pipeline for the node, hence freeing terminal space, running in the background
 bash /scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/rq2/master_pipeline/RQ2_Q8/master_cytbx_rq2_q8.sh
