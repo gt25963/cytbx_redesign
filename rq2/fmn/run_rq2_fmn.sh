09e948c1fa97f9af235b9ac2c3aeb03f50f418f1
gt25963 <gt25963@bristol.ac.uk>
2026-08-31 20:41:28 +0100

    Update run_rq2_fmn.sh with sbatch submission comment
    Added comment explaining sbatch submission for RQ2 pipeline.

--- rq2/fmn/run_rq2_fmn.sh
+++ rq2/fmn/run_rq2_fmn.sh
@@ -6,4 +6,6 @@
 #SBATCH --nodes=1
 #SBATCH --mem=8GB
 #SBATCH --output=/scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/logs/master_RQ2_FMN_%j.out
+
+# Again, can submit the main pipelien (RQ2) as a sbatch, therefore on node, thus freeing up terminal space 
 bash /scratch/b5ae/mvg2713124.b5ae/cytbx_pipeline/rq2/master_pipeline/RQ2_FMN/master_cytbx_rq2_fmn.sh
